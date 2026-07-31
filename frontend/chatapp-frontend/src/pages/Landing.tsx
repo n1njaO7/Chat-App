f75b81ca0c9b995bb81f32940bbb3e2e325b203b
Ashmit Srivastava <157968662+n1njaO7@users.noreply.github.com>
2026-07-31 22:28:36 +0530

    Update

--- frontend/chatapp-frontend/src/pages/Landing.tsx
+++ frontend/chatapp-frontend/src/pages/Landing.tsx
@@ -63,7 +63,7 @@ const Landing = () => {
             <div className="w-2 h-2 bg-green-400 rounded-full animate-bounce delay-200"></div>
           </div>
         </div>
-        <h1 className='text-zinc-500 pt-10 font-doto'>made with love by manish</h1>
+        <h1 className='text-zinc-500 pt-10 font-doto'>made with love by N1njA</h
       </div>
       </div>
     </div>
